311d1730d186fb0a0824fad0973c08ee16954724
LeilaITalbot <43556527+LeilaITalbot@users.noreply.github.com>
2021-02-03 17:55:00 -0800

    Day 1 pt 1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,8 +2,18 @@ import React, { Component, useState } from "react";
 import "../css/App.css";
 import data from "../sample_data.json";
 
+function Question(props) {
+  return <h1 className="header">{props.text}</h1>;
+}
+
 function App() {
-  return <div className="app">Trivia!</div>;
+  let questionIndex = 0;
+  return (
+    <div>
+      <h1 className="header">Trivia!</h1>
+      <Question text={data[questionIndex].question.text} />
+    </div>
+  );
 }
 
 export default App;

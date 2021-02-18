0938e2aa7bf7f1608f11c2fe38b653bf287c11e8
Anquilis <43358398+Anquilis@users.noreply.github.com>
2021-02-17 18:59:44 -0800

    day2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,16 +2,49 @@ import React, { Component, useState } from "react";
 import "../css/App.css";
 import data from "../sample_data.json";
 
+function Answer(props) {
+  return <p>{props.text}</p>;
+}
+
+function NextQuestion() {
+  return <button>Next Question</button>;
+}
+
 function Question(props) {
-  return <h1 className="header">{props.text}</h1>;
+  return (
+    <div>
+      <h1>{props.text}</h1>
+      {props.choices.map((choice) => {
+        return <Answer text={choice} />;
+      })}
+    </div>
+  );
 }
 
 function App() {
   let questionIndex = 0;
+  let questionData = data[questionIndex];
+
+  let questionChoices = questionData.question.choices;
+  let correctChoiceIndex = questionData.question.correct_choice_index;
+  let question = questionData.question.text;
+
+  let [isAnswered, setIsAnswered] = useState("Answer goes here");
   return (
     <div>
-      <h1 className="header">Trivia!</h1>
-      <Question text={data[questionIndex].question.text} />
+      <h1>Trivia!</h1>
+      <Question text={question} choices={questionChoices} />
+      <button
+        onClick={() =>
+          setIsAnswered(
+            "The correct answer is " + questionChoices[correctChoiceIndex]
+          )
+        }
+      >
+        Show Answer
+      </button>
+      <p>{isAnswered}</p>
+      <NextQuestion />
     </div>
   );
 }
